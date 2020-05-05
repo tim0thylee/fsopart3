917a6fa5b9321e216506d2d3fa0167090c75d86e
Timothy Lee <timothylee43@gmail.com>
2020-05-05 14:09:55 -0700

    change for heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function getId (req) {
@@ -7,6 +8,7 @@ morgan.token('body', function getId (req) {
 })
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 
 let persons = [
@@ -80,7 +82,7 @@ app.post('/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
